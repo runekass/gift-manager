8515c64b8fb8322ed4665d69be4f552eb870cf7e
runekass <rune_kasseth@hotmail.com>
2026-03-03 20:12:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 80;
 
 app.use(cors());
 app.use(bodyParser.json());
